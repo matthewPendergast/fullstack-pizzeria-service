d3fec3b12b2ae0cf6e36d824e9c541a640b27169
lurkGH <lurkoasistwitch@gmail.com>
2025-06-10 00:02:36 -0500

    chore: update .env.example files; modify DB reset script; minor project setup

--- scripts/reset-db.sh
+++ scripts/reset-db.sh
@@ -7,8 +7,9 @@ export $(grep -v '^#' .env | xargs)
 DB_NAME=${POSTGRES_DB:-pizza-store}
 POSTGRES_USER=${POSTGRES_USER:-postgres}
 POSTGRES_PASSWORD=${POSTGRES_PASSWORD:-password}
+POSTGRES_HOST=${POSTGRES_HOST:-localhost}
 SCHEMA_FILE="./api/db/init/01-schema.sql"
 SEED_FILE="./api/db/init/02-seed.sql"
 
-PGPASSWORD="$POSTGRES_PASSWORD" psql -U "$POSTGRES_USER" -h localhost -d "$DB_NAME" -f "$SCHEMA_FILE"
-PGPASSWORD="$POSTGRES_PASSWORD" psql -U "$POSTGRES_USER" -h localhost -d "$DB_NAME" -f "$SEED_FILE"
+PGPASSWORD="$POSTGRES_PASSWORD" psql -U "$POSTGRES_USER" -h "$POSTGRES_HOST" -d "$DB_NAME" -f "$SCHEMA_FILE"
+PGPASSWORD="$POSTGRES_PASSWORD" psql -U "$POSTGRES_USER" -h "$POSTGRES_HOST" -d "$DB_NAME" -f "$SEED_FILE"
