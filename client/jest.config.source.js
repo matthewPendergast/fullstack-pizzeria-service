5c4f04843f2958d2bd182b72317fad8b98829dbd
lurkGH <lurkoasistwitch@gmail.com>
2025-06-19 05:39:34 -0500

    chore: update config files

--- client/jest.config.source.js
+++ client/jest.config.source.js
@@ -4,6 +4,9 @@ const config = {
 	testEnvironment: "jsdom",
 	setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
 	testMatch: ["**/__tests__/**/*.test.ts", "**/?(*.)+(test).ts?(x)"],
+	transform: {
+		"^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.test.json" }],
+	},
 };
 
 export default config;
